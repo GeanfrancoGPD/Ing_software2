7e1bef8a1ef60e7c56078f9facaad1370d8e3ccd
Geanfranco <geanfranco1212@gmail.com>
2025-11-01 14:38:26 -0400

    fix: cambios basicos

--- frontend/src/app/pages/principal/principal.page.ts
+++ frontend/src/app/pages/principal/principal.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class PrincipalPage implements OnInit {
 
