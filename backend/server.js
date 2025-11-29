7b2d0deac701f4551b6bd356ca1abc61e1e74160
Geanfranco <geanfranco1212@gmail.com>
2025-11-29 11:32:42 -0400

    fix: cambio en la ruta del cord de Options

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Asegurar respuesta a preflight (OPTIONS)
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json());
 app.use(
